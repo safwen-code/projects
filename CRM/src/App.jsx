17790373d658ab036f27a69c0f109ae8d70ea46b
safwen-code <safwendjebbi1234@gmail.com>
2026-02-06 17:17:38 +0100

    update ui routes

--- CRM/src/App.jsx
+++ CRM/src/App.jsx
@@ -1,34 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+import { Routes, Route, Navigate } from 'react-router-dom'
+import Login from './ui/Login'
+import Product from './ui/Product'
+import Invoice from './ui/Invoice'
+import ProtectedRoute from './ui/ProtectedRoute'
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Routes>
+      {/* login */}
+      <Route path="/login" element={<Login />} />
+      {/* products */}
+      <Route
+        path="/products"
+        element={
+          <ProtectedRoute>
+            <Product />
+          </ProtectedRoute>
+        }
+      />
+      {/* invoice */}
+      <Route
+        path="/invoice"
+        element={
+          <ProtectedRoute>
+            <Invoice />
+          </ProtectedRoute>
+        }
+      />
+
+      <Route path="*" element={<Navigate to="/products" replace />} />
+    </Routes>
   )
 }
 
--- CRM/src/main.jsx
+++ CRM/src/main.jsx
@@ -2,9 +2,18 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { BrowserRouter } from 'react-router-dom'
+import { store } from './store.js'
+import { Provider } from 'react-redux'
+
+import 'devextreme/dist/css/dx.light.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </StrictMode>,
 )
