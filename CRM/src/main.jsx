5350c4cf746e2d4f68800ab963b3997637a35b1d
safwen-code <safwendjebbi1234@gmail.com>
2026-02-06 23:10:14 +0100

    add css react grid

--- CRM/src/main.jsx
+++ CRM/src/main.jsx
@@ -5,9 +5,13 @@ import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import { store } from './store.js'
 import { Provider } from 'react-redux'
+import 'ag-grid-community/styles/ag-grid.css'
+import 'ag-grid-community/styles/ag-theme-alpine.css'
+import 'ag-grid-community/styles/ag-grid.css'
+import 'ag-grid-community/styles/ag-theme-alpine.css'
+import { ModuleRegistry, AllCommunityModule } from 'ag-grid-community'
 
-import 'devextreme/dist/css/dx.light.css'
-
+ModuleRegistry.registerModules([AllCommunityModule])
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
