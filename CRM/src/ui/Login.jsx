79c26a0f229326b11e3ec9d86b858ffda4a83777
safwen-code <safwendjebbi1234@gmail.com>
2026-02-06 22:32:20 +0100

    fix ui

--- CRM/src/ui/Login.jsx
+++ CRM/src/ui/Login.jsx
@@ -7,9 +7,9 @@ import {
   Typography,
 } from '@mui/material'
 import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 import { useAppDispatch } from '../app/hooks.js'
 import { login } from '../Reducer/auth/authSlice.js'
-import { useNavigate } from 'react-router-dom'
 
 const Login = () => {
   const [username, setUsername] = useState('admin')
@@ -27,31 +27,52 @@ const Login = () => {
 
   return (
     <Box
-      sx={{ minHeight: '100vh', display: 'grid', placeItems: 'center', p: 2 }}
+      sx={{
+        position: 'fixed',
+        inset: 0, // top: 0, right: 0, bottom: 0, left: 0
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        p: 2,
+        bgcolor: '#f5f5f5',
+      }}
     >
-      <Card sx={{ width: 420, borderRadius: 3 }}>
+      <Card
+        sx={{
+          width: '100%',
+          maxWidth: 420,
+          borderRadius: 3,
+          boxShadow: 4,
+        }}
+      >
         <CardContent>
-          <Typography variant="h5" fontWeight={700} mb={2}>
+          <Typography variant="h5" fontWeight={700} mb={2} textAlign="center">
             Login
           </Typography>
 
           <TextField
             fullWidth
             label="Username"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
             sx={{ mb: 2 }}
+            onChange={(e) => setUsername(e.target.value)}
           />
+
           <TextField
             fullWidth
             label="Password"
             type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
             sx={{ mb: 2 }}
+            onChange={(e) => setPassword(e.target.value)}
           />
 
-          <Button fullWidth variant="contained" onClick={handleLogin}>
+          <Button
+            fullWidth
+            variant="contained"
+            size="large"
+            onClick={(e) => {
+              handleLogin(e)
+            }}
+          >
             Sign in
           </Button>
         </CardContent>
