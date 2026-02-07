f41e2d147844bbae0a9c6a59b66787d1fe656600
safwen-code <safwendjebbi1234@gmail.com>
2026-02-07 22:59:41 +0100

    remove css

--- CRM/src/main.jsx
+++ CRM/src/main.jsx
@@ -5,13 +5,6 @@ import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import { store } from './store.js'
 import { Provider } from 'react-redux'
-import 'ag-grid-community/styles/ag-grid.css'
-import 'ag-grid-community/styles/ag-theme-alpine.css'
-import 'ag-grid-community/styles/ag-grid.css'
-import 'ag-grid-community/styles/ag-theme-alpine.css'
-import { ModuleRegistry, AllCommunityModule } from 'ag-grid-community'
-
-ModuleRegistry.registerModules([AllCommunityModule])
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
