94853095f8f22977b4e585dd3404ec293d51fb5f
safwen-code <safwendjebbi1234@gmail.com>
2026-03-13 00:32:49 +0100

    update ui

--- CRM/src/App.jsx
+++ CRM/src/App.jsx
@@ -1,34 +1,41 @@
-import { Routes, Route, Navigate } from 'react-router-dom'
+import { Routes, Route, Navigate, useLocation } from 'react-router-dom'
 import Login from './ui/Login'
 import Product from './ui/Product'
 import Invoice from './ui/Invoice'
 import ProtectedRoute from './ui/ProtectedRoute'
-const App = () => {
-  return (
-    <Routes>
-      {/* login */}
-      <Route path="/login" element={<Login />} />
-      {/* products */}
-      <Route
-        path="/products"
-        element={
-          <ProtectedRoute>
-            <Product />
-          </ProtectedRoute>
-        }
-      />
-      {/* invoice */}
-      <Route
-        path="/invoice"
-        element={
-          <ProtectedRoute>
-            <Invoice />
-          </ProtectedRoute>
-        }
-      />
+import GlobalDrawer from './ui/GlobalDrawer'
 
-      <Route path="*" element={<Navigate to="/products" replace />} />
-    </Routes>
+const App = () => {
+  const location = useLocation()
+  const hideDrawer = location.pathname === '/login'
+  return (
+    <>
+      <Routes>
+        {/* login */}
+        <Route path="/login" element={<Login />} />
+        {/* products */}
+        <Route
+          path="/products"
+          element={
+            <ProtectedRoute>
+              <Product />
+            </ProtectedRoute>
+          }
+        />
+        {/* invoice */}
+        <Route
+          path="/invoice"
+          element={
+            <ProtectedRoute>
+              <Invoice />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route path="*" element={<Navigate to="/products" replace />} />
+      </Routes>
+      {!hideDrawer && <GlobalDrawer />}
+    </>
   )
 }
 
